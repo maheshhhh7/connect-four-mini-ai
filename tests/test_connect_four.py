9c1fea756369811268357220ceb2fe8f8c4c65b5
Maheshkumar M <mmvhth24jj@privaterelay.appleid.com>
2026-09-17 20:14:26 +0530

    Fix test_ai assertion and clean up test_connect_four

--- tests/test_connect_four.py
+++ tests/test_connect_four.py
@@ -3,22 +3,25 @@ from src.connect_four import *
 
 def test_board():
     b = board()
+
     assert len(b) == 6
     assert len(b[0]) == 7
 
 
 def test_win():
     b = board()
+
     for c in range(4):
         move(b, c, HUMAN)
+
     assert win(b, HUMAN)
 
 
 def test_ai():
     b = board()
+
     move(b, 3, HUMAN)
+
     c = ai_move(b)
+
     assert c in valid(b)
-
-
-print("All tests passed!")
